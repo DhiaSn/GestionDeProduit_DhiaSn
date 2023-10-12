278dfce8e25feffc0771dee885763b7608397686
Dhou <dhiaeddsn@outlook.com>
2023-10-12 14:40:42 +0100

    add implementation client controller

--- src/main/java/tn/esprit/dhou/gestiondeproduit_dhiasn/controllers/ClientControllerImpl.java
+++ src/main/java/tn/esprit/dhou/gestiondeproduit_dhiasn/controllers/ClientControllerImpl.java
@@ -2,7 +2,11 @@ package tn.esprit.dhou.gestiondeproduit_dhiasn.controllers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.dhou.gestiondeproduit_dhiasn.entities.Client;
 import tn.esprit.dhou.gestiondeproduit_dhiasn.services.IClientService;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -10,4 +14,37 @@ import tn.esprit.dhou.gestiondeproduit_dhiasn.services.IClientService;
 public class ClientControllerImpl {
     IClientService _userServ;
 
+    @GetMapping("/")
+    @ResponseBody
+    public List<Client> getClients() {
+        return _userServ.retrieveAllClients();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Client getClientById(@PathVariable long id) {
+        return _userServ.retrieveClient(id);
+    }
+
+    @PostMapping("/")
+    @ResponseBody
+    public Client postClient(@RequestBody Client c) {
+        if(c == null)
+            return null;
+        return _userServ.addClient(c);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public Client putClient(@PathVariable long id, @RequestBody Client c) {
+        if(c == null || id != c.getIdClient())
+            return  null;
+        return _userServ.updateClient(c);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public void deleteClient(@PathVariable long id) {
+        _userServ.removeClient(id);
+    }
 }
